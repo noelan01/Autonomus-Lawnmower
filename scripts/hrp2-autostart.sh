86d1d13d4d311738476ebd11c03051cc63a3d8a4
noelan <n.andersson0110@gmail.com>
2024-02-06 15:35:52 +0100

    aarch64

--- scripts/hrp2-autostart.sh
+++ scripts/hrp2-autostart.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-#enable can
+#enable can UNCOMMENT
 #sudo ip link set can0 up type can bitrate 1000000
 #sudo ifconfig can0 txqueuelen 5000
 
-#activate (might need to change directory to aarch64 instead of x86_x64)
+#activate
 source /opt/ros/galactic/setup.bash
-source /home/mower/Autonomus-Lawnmower/hrp-p2z-open-dist/x86_64/local_setup.bash
+
+# aarch64
+source /home/mower/Autonomus-Lawnmower/hrp-p2z-open-dist/aarch64/local_setup.bash
+
+# x86_x64
+#source /home/mower/Autonomus-Lawnmower/hrp-p2z-open-dist/x86_64/local_setup.bash
+
 
 #the keepalive launch file will make the mower stay awak, it is good when developing, but will drain battery if mower stops in garden.
 ros2 launch hrp_pkg hrp_keepalive.launch.py
