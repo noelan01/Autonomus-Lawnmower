705e2b6c48a91a25e6cbad0b32c3a8f23e28f93a
RosBox <n.andersson01@hotmail.com>
2024-02-06 11:44:19 +0100

    keyboard autostart

--- scripts/hrp2-autostart.sh
+++ scripts/hrp2-autostart.sh
@@ -4,7 +4,7 @@
 #sudo ip link set can0 up type can bitrate 1000000
 #sudo ifconfig can0 txqueuelen 5000
 
-#activate
+#activate (might need to change directory to aarch64 instead of x86_x64)
 source /opt/ros/galactic/setup.bash
 source /home/mower/Autonomus-Lawnmower/hrp-p2z-open-dist/x86_64/local_setup.bash
 
--- scripts/keyboard-autostart.sh
+++ scripts/keyboard-autostart.sh
@@ -1,1 +1,6 @@
-# write keyboard autostart here
+#!/bin/bash
+
+source /opt/ros/galactic/setup.bash
+source /home/mower/Autonomus-Lawnmower/hrp-p2z-open-dist/x86_64/local_setup.bash
+
+python3 /home/mower/Autonomus-Lawnmower/hrp-p2z-open-dist/x86_64/lib/python3.8/site-packages/hqv_keyboard_remote_drive/keyboard_remote_drive_node.py 
