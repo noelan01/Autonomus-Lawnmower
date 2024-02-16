4f7adf38fca611581524fe1fc51b0b06e421ea5f
noelan <n.andersson0110@gmail.com>
2024-02-16 13:21:43 +0100

    uppdaterat getters och setters

--- src/kalman.py
+++ src/kalman.py
@@ -1,12 +1,10 @@
-# Kalman filter to estimate position and velocity
+###################################################################
+"""
+EXTENDED sKALMAN FILTER
+"""
+###################################################################
 
 import numpy as np
-
-###################################################################
-"""
-EXTENDED KALMAN FILTER
-"""
-###################################################################
 
 class EKF():
     def __init__(self, init_state, init_input, init_P, init_noise, init_pos_reading):
@@ -15,7 +13,26 @@ class EKF():
         self._P = init_P                       # init_P = [[0.1, 0, 0],[0, 0.1, 0],[0, 0, 0.1]] från exempel
         self._noise = init_noise
         self._Z_k = init_pos_reading
-    
+
+        self._B = None
+        self._K_k = None
+        self._y_k = None
+        self._H_k = np.eye(3)   # justera vid behov
+        self._R_k = np.eye(3)   # justera vid behov
+        self._F_k = np.eye(3)
+        self._Q_k = np.eye(3)   # kovariansmatris, se anteckningar JUSTERA VID BEHOV
+        self._dk = ...          # VAD ÄR DK I VÅRT FALL?
+
+    def kalman_update(self):
+        # TODO
+        """
+            denna funktion uppdaterar till nästa state
+            och går igenom alla steg som skrivits i anteckningar.
+
+            Möjligtvis gör detta i uppdate funktionen
+        """
+        pass
+
     def predict_state(self, input_prev):
         B = self.get_B()
         A = self.get_A()
@@ -24,54 +41,84 @@ class EKF():
         noise = self.get_noise()
 
         self.state = A @ state_prev + B @ input_prev + noise
-        return self.state
     
-    def predict_cov(self):
-        F_k = np.eye(3)
-        Q_k = np.eye(3)             # kovariansmatris, se anteckningar JUSTERA VID BEHOV
 
-        P = F_k @ self._P @ np.transpose(F_k) + Q_k
-        return P
+    def predict_cov(self):
+        F_k = self.get_F_k()
+        Q_k = self.get_Q_k()
+
+        self._P = F_k @ self._P @ np.transpose(F_k) + Q_k
+
     
-    def gain(self):         # steg 4 till 6 
-        H_k = np.eye(3)             # justera vid behov
-        R_k = np.eye(3)             # justera vid behov
-        P_k = self.predict_cov()
+    def gain(self): 
+        H_k = self.get_H_k()
+        R_k = self.get_R_k()
+
+        self.predict_cov()
+        P_k = self.get_cov()
         Z_k = self.get_pos_reading()                   # mätvärden [x_k, y_k, theta_k]
-        X_k = self.predict_state()
+        
+        self.predict_state()
+        X_k = self.get_state()
         sensor_error = self.get_sensor_error()
 
-        y_k = Z_k - H_k @ X_k + sensor_error
+        self._y_k = Z_k - H_k @ X_k + sensor_error
 
         S_k = H_k @ P_k @ np.transpose(H_k) + R_k
 
-        K_k = P_k @ np.transpose(H_k) @ np.linalg.inv(S_k)
-        return K_k
-
-    def get_B(self, theta_prev, dk):
-        return np.array([[np.cos(theta_prev) * dk, 0],
-                        [np.sin(theta_prev) * dk, 0],
-                        [0,                      dk]])
+        self._K_k = P_k @ np.transpose(H_k) @ np.linalg.inv(S_k)
     
     def update(self):
-        # TODO
-        # steg 7-8
-        # uppdatera state och cov
-        return None
+        state_prev = self.get_state()
+        K_k = self.get_K_k()
+        y_k = self.get_y_k()
+        P_prev = self.get_cov()
+        H_k = self.get_H_k()
+
+        self._state = state_prev + K_k @ y_k        # uppdatera state
+        
+        self._P = (np.eye(3) - K_k @ H_k) @ P_prev  # uppdatera cov
 
     def pos_update(self):
         # TODO
         # Hämta positionsmätningar och uppdatera Z_k
         self._Z_k = ...
 
+    def set_B(self):
+        theta_prev = self.get_theta()
+        dk = self.get_dk()
+
+        self._B = np.array([[np.cos(theta_prev) * dk, 0],
+                        [np.sin(theta_prev) * dk, 0],
+                        [0,                      dk]])
+
+    ######   ######   #######   #######    ######    #######    ######
+    #        #           #         #       #         #     #    #
+    #  ###   ###         #         #       ###       #######    ######
+    #    #   #           #         #       #         # ##            #
+    ######   ######      #         #       ######    #   ##     ######
+        
     def get_A(self):
         return np.eye(3)
+    
+    def get_B(self, theta_prev, dk):
+        self.set_B()
+        return self._B
+    
+    def get_K_k(self):
+        return self._K_k
     
     def get_state(self):
         return self._state
     
+    def get_cov(self):
+        return self._P
+    
     def get_input(self):
         return self._input
+    
+    def get_y_k(self):
+        return self._y_k
     
     def get_noise(self):
         return self._noise
@@ -79,11 +126,35 @@ class EKF():
     def get_pos_reading(self):
         return self._Z_k
     
+    def get_H_k(self):
+        return self._H_k
+    
+    def get_R_k(self):
+        return self._R_k
+    
+    def get_F_k(self):
+        return self._F_k
+    
+    def get_Q_k(self):
+        return self._Q_k
+    
+    def get_dk(self):
+        return self._dk
+    
+    def get_theta(self):
+        # TODO
+        # write function that gets angle of lawnmower (Z_k)
+        pass
+    
     def get_sensor_error(self):
         # TODO
         # hämta sensorerror fån ROS2
         error = ...
         return error
+    
+    
+
+
 
 
 ###################################################################
@@ -131,54 +202,3 @@ class old_EKF:
         K = self.P @ H.T @ np.linalg.inv(S)
         self.state += K @ y
         self.P = (np.eye(3) - K @ H) @ self.P
-
-
-
-########################################
-"""
-REGULAR KALMAN FILTER
-not adjusted to our model
-"""
-########################################
-
-class Kalman_filter():
-    def __init__(self) -> None:
-        pass
-
-    def kf_predict(X, P, A, Q, B, U):
-        X = np.dot(A, X) + np.dot(B, U)
-        P = np.dot(A, np.dot(P, A.T)) + Q
-        return(X,P)
-
-
-    def kf_update(X, P, Y, H, R):
-        IM = np.dot(H, X)
-        IS = R + np.dot(H, np.dot(P, H.T))
-        K = np.dot(P, np.dot(H.T, np.linalg.inv(IS)))
-        X = X + np.dot(K, (Y-IM))
-        P = P - np.dot(K, np.dot(IS, K.T))
-        LH = gauss_pdf(Y, IM, IS)
-        return (X,P,K,IM,IS,LH)
-
-
-    def gauss_pdf(X, M, S):
-        if M.shape()[1] == 1:
-            DX = X - np.tile(M, X.shape()[1])
-            E = 0.5 * sum(DX * (np.dot(np.linalg.inv(S), DX)), axis=0)
-            E = E + 0.5 * M.shape()[0] * np.log(2 * np.pi) + 0.5 * np.log(np.linalg.det(S))
-            P = np.exp(-E)
-
-        elif X.shape()[1] == 1:
-            DX = np.tile(X, M.shape()[1])- M
-            E = 0.5 * sum(DX * (np.dot(np.linalg.inv(S), DX)), axis=0)
-            E = E + 0.5 * M.shape()[0] * np.log(2 * np.pi) + 0.5 * np.log(np.linalg.det(S))
-            P = np.exp(-E)
-
-        else:
-            DX = X-M
-            E = 0.5 * np.dot(DX.T, np.dot(np.linalg.inv(S), DX))
-            E = E + 0.5 * M.shape()[0] * np.log(2 * np.pi) + 0.5 * np.log(np.linalg.det(S))
-            P = np.exp(-E)
-
-        return (P[0],E[0])
-    
