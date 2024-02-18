0cfb29d888a3df91e8e6c6b6030e9ece150b0744
noelan <n.andersson0110@gmail.com>
2024-02-18 11:50:19 +0100

    Kalman i stort sätt färdigtskrivet. Lite justeringar kring info från HRP2 osv krävs

--- src/kalman.py
+++ src/kalman.py
@@ -10,66 +10,31 @@ class EKF():
     def __init__(self, init_state, init_input, init_P, init_noise, init_pos_reading):
         self._state = init_state               # init_state = [x_prev, y_prev, theta_prev]
         self._input = init_input               # init_input = [v_prev, yaw_rate_prev]
-        self._P = init_P                       # init_P = [[0.1, 0, 0],[0, 0.1, 0],[0, 0, 0.1]] från exempel
         self._noise = init_noise
         self._Z_k = init_pos_reading
 
         self._B = None
         self._K_k = None
         self._y_k = None
+
+        self._P = np.array([[0.1, 0, 0], [0, 0.1, 0], [0, 0, 0.1]])     # från exempel JUSTERA
         self._H_k = np.eye(3)   # justera vid behov
         self._R_k = np.eye(3)   # justera vid behov
         self._F_k = np.eye(3)
         self._Q_k = np.eye(3)   # kovariansmatris, se anteckningar JUSTERA VID BEHOV
         self._dk = ...          # VAD ÄR DK I VÅRT FALL?
 
-    def kalman_update(self):
-        # TODO
-        """
-            denna funktion uppdaterar till nästa state
-            och går igenom alla steg som skrivits i anteckningar.
-
-            Möjligtvis gör detta i uppdate funktionen
-        """
-        pass
-
-    def predict_state(self, input_prev):
-        B = self.get_B()
-        A = self.get_A()
-        state_prev = self.get_state()
-        input_prev = self.get_input()
-        noise = self.get_noise()
-
-        self.state = A @ state_prev + B @ input_prev + noise
-    
-
-    def predict_cov(self):
-        F_k = self.get_F_k()
-        Q_k = self.get_Q_k()
-
-        self._P = F_k @ self._P @ np.transpose(F_k) + Q_k
-
-    
-    def gain(self): 
-        H_k = self.get_H_k()
-        R_k = self.get_R_k()
-
-        self.predict_cov()
-        P_k = self.get_cov()
-        Z_k = self.get_pos_reading()                   # mätvärden [x_k, y_k, theta_k]
-        
-        self.predict_state()
-        X_k = self.get_state()
-        sensor_error = self.get_sensor_error()
-
-        self._y_k = Z_k - H_k @ X_k + sensor_error
-
-        S_k = H_k @ P_k @ np.transpose(H_k) + R_k
-
-        self._K_k = P_k @ np.transpose(H_k) @ np.linalg.inv(S_k)
-    
+    """
+    Update funtionen uppdaterar alla delar i kalmanfiltret.
+    Kalla på denna för att få den uppdaterade state estimationen.
+    """    
     def update(self):
-        state_prev = self.get_state()
+        self.predict_state()            # 1. predict state
+        self.predict_cov()              # 2. predict cov
+        self.set_gain()                 # 3. - 5. set optimal gain
+
+
+        state_prev = self.get_state()   # 6. & 7. Updatera state och cov
         K_k = self.get_K_k()
         y_k = self.get_y_k()
         P_prev = self.get_cov()
@@ -79,10 +44,58 @@ class EKF():
         
         self._P = (np.eye(3) - K_k @ H_k) @ P_prev  # uppdatera cov
 
-    def pos_update(self):
+        return self.get_state()                     # returna uppdaterat state
+
+    ######   ######   #######   #######    ######    #######    ######
+    #        #           #         #       #         #     #    #
+    ######   ###         #         #       ###       #######    ######
+         #   #           #         #       #         # ##            #
+    ######   ######      #         #       ######    #   ##     ######
+        
+
+    def predict_state(self, input_prev):
+        self.set_B()
+        B = self.get_B()
+        A = self.get_A()
+        state_prev = self.get_state()
+        input_prev = self.get_input()
+        noise = self.get_noise()
+
+        self._state = A @ state_prev + B @ input_prev + noise
+    
+
+    def predict_cov(self):
+        F_k = self.get_F_k()
+        Q_k = self.get_Q_k()
+        P_prev = self.get_P()
+
+        self._P = F_k @ P_prev @ np.transpose(F_k) + Q_k
+
+    
+    def set_gain(self): 
+        H_k = self.get_H_k()
+        R_k = self.get_R_k()
+
+        P_k = self.get_cov()
+
+        self.set_pos_update()
+        Z_k = self.get_pos_reading()                   # mätvärden [x_k, y_k, theta_k]
+        
+        X_k = self.get_state()
+        sensor_error = self.get_sensor_error()
+
+        self._y_k = Z_k - H_k @ X_k + sensor_error
+
+        S_k = H_k @ P_k @ np.transpose(H_k) + R_k
+
+        self._K_k = P_k @ np.transpose(H_k) @ np.linalg.inv(S_k)
+    
+
+    def set_pos_update(self):
         # TODO
         # Hämta positionsmätningar och uppdatera Z_k
         self._Z_k = ...
+        
 
     def set_B(self):
         theta_prev = self.get_theta()
@@ -104,6 +117,9 @@ class EKF():
     def get_B(self, theta_prev, dk):
         self.set_B()
         return self._B
+    
+    def get_P(self):
+        return self._P
     
     def get_K_k(self):
         return self._K_k
@@ -142,9 +158,10 @@ class EKF():
         return self._dk
     
     def get_theta(self):
-        # TODO
-        # write function that gets angle of lawnmower (Z_k)
-        pass
+        self.set_pos_update()
+        if self._Z_k == None:
+            return 0
+        return self._Z_k[2]
     
     def get_sensor_error(self):
         # TODO
