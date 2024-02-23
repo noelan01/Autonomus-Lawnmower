c12cc987046fda0de405ab113500bf90dbd8272e
antonmark00 <antonm.mark@icloud.com>
2024-02-23 11:11:57 +0100

    utkast transformation

--- scripts/coord_transformation.py
+++ scripts/coord_transformation.py
@@ -1,15 +1,13 @@
 #referensstationen är satt som origo i ett fast globalt koordninatsystem på gräsmattan. x pekar norr, y pekar väst
 #Gräsklipparen är satt som ett dynamiskt lokalt koordinatsystem, x pekar frammåt, y pekar vänster
-#topic hqv_mower/gnss_rtk/rel_enu ger gräsklipparens position i norr/syd relativ referensstation
+#topic hqv_mower/gnss_rtk/rel_enu ger gräsklipparens position i norr/syd relativ referensstation, mower_xy_global
 import numpy as np
 
-
-#mower_xy_global = np.array([0,0,0])     #Mowers postition i det globala systemet, fås från topic
-
-
+yaw_angle = 0
+yaw_angle = yaw_angle % (2*np.pi)
 offset_angle = 0    #offset_angle skillnaden i vinkel mellan norr i globala systemet och IMU, positiv medurs(fås från kalibrering)
-
-
+#Kalibrering kan fås genom att köra rakt fram och se ta ut rtk data, trigonometri kan ge vinkeln i det globala systemet som
+#mowern kör i. IMU vinkeln jämförs och en offset kam fås ut
 def rotation_matrix(angle):
     return np.array([
         [np.cos(angle),  -np.sin(angle), 0],
@@ -50,8 +48,3 @@ def goal_global_to_local(offset_angle,goal_xy_global):      #tar in Målkordinat
 
 
 
-
-
-
-
-# Kolla denna och jämför accuracy med rtk accuracy: hqv_mower/gnss/position/acc
