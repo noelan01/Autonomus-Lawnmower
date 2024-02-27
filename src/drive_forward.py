1b157a9facc25737987fb5c05a0393ccf88ae607
elm4n <elmer.janmark@gmail.com>
2024-02-27 14:59:48 +0100

    drive_forward

--- src/drive_forward.py
+++ src/drive_forward.py
@@ -1,12 +1,15 @@
 import rclpy
 import threading
 import time
+import sys
+sys.path.append("./hrp-p2z-open-dist/aarch64/lib/python3.8/site-packages")
 
 from rclpy.node import Node
-from hqv_public_interface.msg import RemoteDriverDriveCommand
+from hqv_public_interface.msg import RemoteDriverDriveCommand #kanske måste lägga annan filepath
 from hqv_public_interface.msg import MowerImu
 
 class HQVMowerController(Node):
+    """ To test publishing on drive topic and subscription on imu yaw topic"""
     def __init__(self):
         super().__init__('hqv_mower_controller')
         self.drive_publisher = self.create_publisher(RemoteDriverDriveCommand, '/hqv_mower/remote_driver/drive', 100)
@@ -28,13 +31,14 @@ class HQVMowerController(Node):
         # Command to drive forward
         self.move(speed=1.0, steering=0.0)
 
-        # Wait for 3 seconds
         start_time = time.time()
+
+        #drive forward for 3 seconds
         while time.time() - start_time < 3.0:
-            self.move(speed=1.0, steering=0.0)
+            self.move(speed=1.0, steering=0.0) #linear speed of 1, straight line expected
             rclpy.spin_once(self, timeout_sec=0.1)  # Process any incoming messages/events
             self.get_logger().info('Publishing message')
-            self.rate.sleep()
+            self.rate.sleep()   #ensure 10hz publish rate
 
         # Stop the movement
         self.move(speed=0.0, steering=0.0)
